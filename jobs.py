1c98216e8a9c84b61a9b1110270c1f25975d62da
Vikas <vlohana103@gmail.com>
2026-05-17 19:46:42 -0400

    added C.R.U.D and jobs class with constructor

---
+++ jobs.py
@@ -0,0 +1,15 @@
+
+
+
+
+class Job_Application:
+    def __init__(self, job_id, company_name, job_title, url, status, date_applied, last_updated): # this is the constructor
+        self.job_id = job_id # this is saying what_is_inputted_here = job_id
+        self.company_name = company_name
+        self.job_title = job_title
+        self.url = url
+        self.status = status
+        self.date_applied = date_applied
+        self.last_updated = last_updated
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import jobs
+
+# C.R.U.D
+tracker = {}
+
+def create():
+    pass
+
+def read():
+    pass
+
+def update():
+    pass
+
+def delete():
+    pass
 
 
+# ============== Core Logic ==============
 
 welcome = "Welcome to the Job Application Tracker"
 print("*" * len(welcome))
@@ -7,13 +24,33 @@ print(welcome)
 print("*" * len(welcome))
 print()
 
-
 while True:
     print("*** Main Menu ***")
     enter = input("Press Y to begin or N to exit\n").lower()
 
     if enter == "y":
-        pass
+        while True:
+            print("*** Using Job Application Tracker ***")
+            ask = input("Press:\n 1 to Add\n 2 to View\n 3 to Update\n 4 to Delete\n 5 to Return to the Main Menu?\n")
+            #C.R.U.D
+            if ask == "1":
+                pass
+            # Create
+            elif ask == "2":
+                pass
+            # Read
+            elif ask == "3":
+                pass
+            # Update
+            elif ask == "4":
+                pass
+            # Delete
+            elif ask == "5":
+                print("Returning to Main Menu...\n")
+                break
+            else:
+                print("Invalid Input... \n")
+
     elif enter == "n":
         print("Exiting the Job Application Tracker...")
         break
